19012944a5e59a260f69a6f77c42a28ba1ceb24e
kuoc0321 <97783383+kuoc0321@users.noreply.github.com>
2024-02-26 09:19:30 +0700

    Add files via upload

---
+++ Project22/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $user = [
+            "id" => 1,
+            "name" => "John Doe",
+            "email" => "johndoe@example.com",
+            "avatar" => "uploads/default_avatar.png" // Initial avatar URL
+           ];
+    ?>
+    <form action="admin/update_profile.php" method="post" enctype="multipart/form-data">
+        <h2>Profile Information</h2>
+        <label for="name">Name:</label>
+        <input type="text" name="name" value="<?php echo $user['name']; ?>"
+        required>
+        <br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" value="<?php echo $user['email']; ?>"
+        disabled>
+        <br>
+        <label for="avatar">Avatar:</label>
+        <input type="file" name="avatar" accept="image/*">
+        <br>
+        <button type="submit">Update Profile</button>
+</form>
+
+</body>
+</html>
